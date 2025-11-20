86d0108899a11f7ed02872d7869ca5f90054842a
Eduardo-10us <duduramos047@gmail.com>
2025-11-19 21:44:21 -0300

    Add files via upload

--- Aula4novo1.py
+++ Aula4novo1.py
@@ -1,3 +1,11 @@
 lista1=[]
 lista1.append(1)
 print(lista1)
+
+lista1.append(2)
+lista1.append(3)
+print(lista1)
+
+print("indice 0:" + str(lista1[0]))
+print("indice 1:" + str(lista1[1]))
+print("indice 2:" + str(lista1[2]))
