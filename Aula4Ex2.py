9a494c0f3104f9decb683cd0068f4890a714a015
Eduardo-10us <duduramos047@gmail.com>
2025-11-19 21:19:29 -0300

    Add files via upload

--- Aula4Ex2.py
+++ Aula4Ex2.py
@@ -6,28 +6,25 @@ resp = input("Entrar no cadastro do dia? Digite S para sim ou qualquer tecla par
 
 while resp == "S":
     nome = input("Nome do paciente: ")
-    idade = int (input("Idade do paciente: "))
+    idade = input("Idade do paciente: ")
     medico = input("Nome do médico: ")
-    especialidade = input("Especialidade (P = Pediatria/ C = Clínica médica): ").strip().upper()
-
-    total_pacientes += 1
+    especialidade = input("Especialidade P = Pediatria - C = Clínica médica: ").strip().upper()
 
     if especialidade == "P":
         total_pediatria += 1
     elif especialidade == "C":
         total_clinica += 1
     else:
-        print("Especialidade inválida - contado apenas no total geral.")
-    continue
+        print("Especialidade inválida")
+        continue
 
-
+    total_pacientes += 1
 
     resp = input("Continuar no cadastro? Digite S para sim ou qualquer tecla para não: ").strip().upper()
 
 print(f"Quantidade total de pacientes: {total_pacientes}")
 print(f"Quantidade de pacientes na Clínica Médica: {total_clinica}")
 print(f"Quantidade de pacientes na Pediatria: {total_pediatria}")
-
 
 
 
