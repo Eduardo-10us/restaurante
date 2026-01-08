38771dacb95c6d6eb94c3c6088cea4ccc369da30
Eduardo-10us <duduramos047@gmail.com>
2026-01-07 21:42:52 -0300

    Add files via upload

--- principal/urls.py
+++ principal/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import inicio
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cliente.urls')),
-    path('', include('produto.urls')),
-    path('', include('principal.urls'))
+   path('',inicio)
 ]
